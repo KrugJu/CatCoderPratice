13db03cbeccc735a27cc142464e516aa2f82c8eb
krug.julian <40146720160317@litec.ac.at>
2018-10-24 23:32:00 +0200

    Block Class basic properties

--- Block.cs
+++ Block.cs
@@ -2,9 +2,21 @@
 
 public class Block
 {
+    public int id;
+    public int x;
+    public int y;
+    public char orientation;
 
-	public Block()
+    public Block(string input)
 	{
-        
+        char[] array = input.ToCharArray();
+        if (array.Length == 5)
+        {
+            id = Int32.Parse(array[0]);
+            orientation = array[1];
+            x = Int32.Parse(array[2]);
+            y = Int32.Parse(array[3]);
+            length = Int32.Parse(array[4]);
+        }
 	}
 }
