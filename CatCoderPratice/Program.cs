09aaf3a33d7cee05e745b1d91af3704fa49d93f9
Berger Elias <40146720160379@litec.ac.at>
2018-10-24 23:14:47 +0200

    asfaf

--- CatCoderPratice/Program.cs
+++ CatCoderPratice/Program.cs
@@ -10,7 +10,12 @@ namespace CatCoderPratice
     {
         static void Main(string[] args)
         {
-
+            int a = 1;
+            int b = 1;
+            int c = 1;
+            int d = 1;
+            int e = 1;
+            int f = 1;
         }
     }
 }
