f09282aa415d751f36e948cceed33066ae746f96
krug.julian <40146720160317@litec.ac.at>
2018-10-24 23:41:36 +0200

    moved blocks.cs

---
+++ CatCoderPratice/Block.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Block
+{
+    public int id;
+    //x and y coordinates of the bottom left half of the block
+    public int x;
+    public int y;
+    public char orientation;
+    public int length;
+
+    public Block(int id,char orientation,int x, int y,int length)
+	{
+        this.id = id;
+        this.orientation = orientation;
+        this.x = x;
+        this.y = y;
+        this.length = length;
+	}
+}
