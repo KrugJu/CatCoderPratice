35309772bd828fdde541968b864584d286b6635e
Berger Elias <40146720160379@litec.ac.at>
2018-10-24 23:48:41 +0200

    asd

---
+++ CatCoderPratice/Splitter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatCoderPratice
+{
+    static class Splitter
+    {
+        public static string[] Split(string s)
+        {
+            string[] values = s.Split(' ');
+
+            List<string[]> valuesBlocked = new List<string[]>();
+            
+            return null;
+        }
+    }
+}
