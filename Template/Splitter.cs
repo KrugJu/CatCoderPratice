7c835aca7d017283a4d45b2a9b3ea3fd733855a4
Berger Elias <40146720160379@litec.ac.at>
2018-11-16 10:45:00 +0100

    xd

--- Template/Splitter.cs
+++ Template/Splitter.cs
@@ -21,5 +21,38 @@ namespace Template
                       
             return valuesList;
         }
+
+        public static int[][] SplitIntoMatrix(string s)
+        {
+            string[] rows = s.Split('\n');
+            string[][] grid = new string[rows.Length][];
+            
+            for (int row = 1; row < rows.Length; row++)
+            {
+                grid[row] = rows[row].Split(' ');
+            }
+
+            int[][] intGrid = new int[rows.Length][];
+
+            for (int row = 0; row < rows.Length; row++)
+            {
+                for(int col = 0; col < grid[0].Length; col++)
+                {
+                    intGrid[row][col] = Convert.ToInt32(grid[row][col]);
+                }
+            }
+
+            return intGrid;
+        }
+
+
+        public static List<int> GetRowsCols(string s)
+        {
+            string[] values = s.Split(' ', '\n');
+            List<int> valuesList = new List<int>();
+            valuesList.Add(Convert.ToInt32(values[0]));
+            valuesList.Add(Convert.ToInt32(values[1]));
+            return valuesList;
+        }
     }
 }
