2c66b0abfeef472a48785a95733d8d4d1687ce2f
KrugJu <40146720160317@litec.ac.at>
2018-11-16 10:08:17 +0100

    Added Processor LEVEL1

--- Template/Processor.cs
+++ Template/Processor.cs
@@ -9,10 +9,12 @@ namespace Template
     public static class Processor
     {
 
-        public static  x()
+        public static int TestForBuilding(List<int> values)
         {
-
-
+            values = new List<int>();
+            List<int> buildings = values.Where(x => x > 0).ToList();
+            if (buildings.Count > 0) return 1;
+            else return 0;
         }
     }
 }
