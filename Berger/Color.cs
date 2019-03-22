c5f3e38c546dc83af7e4fdcddc25279b822d9093
Berger Elias <40146720160379@litec.ac.at>
2019-03-22 10:27:24 +0100

    x

---
+++ Berger/Color.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Berger
+{
+    public class Color
+    {
+        public int R;
+        public int G;
+        public int B;
+
+        public Color(int r, int g, int b)
+        {
+            this.R = r;
+            this.G = g;
+            this.B = b;
+        }
+
+        public int getDistance(Color c)
+        {
+            return (int) Math.Sqrt(Math.Pow(this.R - c.R, 2) + Math.Pow(this.G - c.G, 2) + Math.Pow(this.B - c.B, 2));
+        }
+    }
+}
--- Berger/Program.cs
+++ Berger/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,35 @@ namespace Berger
     {
         static void Main(string[] args)
         {
+            int clines;
+            List<List<Color>> colors = new List<List<Color>>();
+            List<int> distances = new List<int>();
+            List<string> sdist = new List<string>();
+
+            using (StreamReader sr = new StreamReader(@"C:\CCC\level1_3.in"))
+            {
+                string line;
+                clines = Convert.ToInt32(sr.ReadLine().Trim());
+
+                for(int i = 0; i < clines; i++)
+                {
+                    List<Color> cList = new List<Color>();
+                    line = sr.ReadLine().Trim();
+                    string[] c = line.Split(' ');
+                    cList.Add(new Color(Convert.ToInt32(c[0]), Convert.ToInt32(c[1]), Convert.ToInt32(c[2])));
+                    cList.Add(new Color(Convert.ToInt32(c[3]), Convert.ToInt32(c[4]), Convert.ToInt32(c[5])));
+                    colors.Add(cList);
+                }
+            }
+
+            foreach (List<Color> pair in colors)
+            {
+                distances.Add(pair[0].getDistance(pair[1]));
+                sdist.Add(pair[0].getDistance(pair[1]).ToString());
+            }
+
+
+            System.IO.File.WriteAllLines(@"C:\CCC\level1_3.ou", sdist);
 
         }
     }
